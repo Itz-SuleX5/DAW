2fb8088fc9871a865ed89926d92d77860d76375d
Itz-SuleX5 <AA23028@ues.edu.sv>
2025-05-25 09:07:30 -0700

    fix: configurar entorno de desarrollo para GitHub Codespaces

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
 
-const customConfig: UserConfigFn = (env) => ({
+const customConfig: UserConfigFn = () => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
   server: {
     host: '0.0.0.0',
     port: 8080,
-    strictPort: true
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: '0.0.0.0',
+      port: 8080
+    }
   }
 });
 
