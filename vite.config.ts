f0b1eb6073c539c7526184f6edac79ed7ba59e09
Itz-SuleX5 <AA23028@ues.edu.sv>
2025-05-25 08:13:29 -0700

    fix: configurar correctamente el entorno de desarrollo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import { overrideVaadinConfig } from './vite.generated';
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 8080,
+      clientPort: 8080
+    }
+  }
 });
 
 export default overrideVaadinConfig(customConfig);
